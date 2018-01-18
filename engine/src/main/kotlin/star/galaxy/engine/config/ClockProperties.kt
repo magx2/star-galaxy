d3825047e7d0c2935893499f06aeb4b7406ebbe8
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-18 20:29:06 +0100

    ClockProperties.kt

---
+++ engine/src/main/kotlin/star/galaxy/engine/config/ClockProperties.kt
@@ -0,0 +1,13 @@
+package star.galaxy.engine.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+import star.galaxy.engine.metainformations.Second
+
+@Configuration
+@ConfigurationProperties(prefix = "engine.clock")
+class ClockProperties {
+    val fps: Int = 60
+    @Second
+    val timeInGame: Int = 1
+}
--- engine/src/main/kotlin/star/galaxy/engine/impl/FpsClock.kt
+++ engine/src/main/kotlin/star/galaxy/engine/impl/FpsClock.kt
@@ -1,29 +1,30 @@
 package star.galaxy.engine.impl
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import star.galaxy.engine.Clock
+import star.galaxy.engine.config.ClockProperties
 import star.galaxy.engine.metainformations.Milli
 import star.galaxy.engine.metainformations.Second
 import java.util.concurrent.TimeUnit
 
 @Service
-internal class FpsClock : Clock {
+internal class FpsClock @Autowired constructor(private val clockProperties: ClockProperties) : Clock {
     @Milli
     @Second
     var timestamp = System.currentTimeMillis()
+    @Suppress("LeakingThis")
     @Milli
     @Second
-    val timeToWait: Long = TimeUnit.SECONDS.toMillis(1) / 60 // 60 FPS
-    @Second
-    val timeInGame = 1
+    val timeToWait: Long = TimeUnit.SECONDS.toMillis(1) / clockProperties.fps
 
     override fun next(): Int {
-        val currentTimeMillis = System.currentTimeMillis()
-        val delta = currentTimeMillis - timestamp
+        @Milli @Second val currentTimeMillis = System.currentTimeMillis()
+        @Milli @Second val delta = currentTimeMillis - timestamp
         if (timeToWait - delta > 0) {
             TimeUnit.MILLISECONDS.sleep(timeToWait - delta)
         }
 
-        return timeInGame
+        return clockProperties.timeInGame
     }
 }
