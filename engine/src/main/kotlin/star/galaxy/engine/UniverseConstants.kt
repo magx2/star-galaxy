083d26d86f1d72a0580f84d0365c614af86f8e28
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-22 23:55:41 +0100

    Binding light year with speed of light

--- engine/src/main/kotlin/star/galaxy/engine/UniverseConstants.kt
+++ engine/src/main/kotlin/star/galaxy/engine/UniverseConstants.kt
@@ -2,8 +2,11 @@ package star.galaxy.engine
 
 import star.galaxy.engine.metainformations.Meter
 import star.galaxy.engine.metainformations.MeterSecond
+import star.galaxy.engine.metainformations.Second
 
 data class UniverseConstants(val G: Double = 6.67408e-11,
                              val PI: Double = Math.PI,
-                             @MeterSecond val speedOfLight: Double = 299_792_458.0,
-                             @Meter val lightYear: Double = 9_460_730_472_580_800.0)
+                             @MeterSecond val speedOfLight: Double = 299_792_458.0) {
+    @Meter
+    val lightYear: Double = speedOfLight * @Second 31_557_600
+}
