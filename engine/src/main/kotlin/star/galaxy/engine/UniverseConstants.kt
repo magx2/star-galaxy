420d11ffb5625e8f9cbafd380b3a1d3c4d905003
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-22 23:51:16 +0100

    Removing not needed service

--- engine/src/main/kotlin/star/galaxy/engine/UniverseConstants.kt
+++ engine/src/main/kotlin/star/galaxy/engine/UniverseConstants.kt
@@ -1,7 +1,9 @@
 package star.galaxy.engine
 
+import star.galaxy.engine.metainformations.Meter
 import star.galaxy.engine.metainformations.MeterSecond
 
 data class UniverseConstants(val G: Double = 6.67408e-11,
                              val PI: Double = Math.PI,
-                             @MeterSecond val speedOfLight: Double = 299_792_458.0)
+                             @MeterSecond val speedOfLight: Double = 299_792_458.0,
+                             @Meter val lightYear: Double = 9_460_730_472_580_800.0)
