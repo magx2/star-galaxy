a26bbfafc6417d586cf933c5ef5b617a73729100
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-22 18:00:02 +0100

    Better assertion

--- engine/src/main/kotlin/star/galaxy/engine/entites/Orb.kt
+++ engine/src/main/kotlin/star/galaxy/engine/entites/Orb.kt
@@ -1,16 +1,16 @@
 package star.galaxy.engine.entites
 
-import org.springframework.util.Assert
 import star.galaxy.engine.metainformations.Meter
 import star.galaxy.engine.types.ForceApplicable
 import star.galaxy.engine.types.SphereShape
 import star.galaxy.engine.types.WithUuid
+import star.galaxy.engine.utils.checkPositive
 
 data class Orb(private val identifiableObject: IdentifiableObject,
                private val objectInSpace: ObjectInSpace,
                @Meter private val radian: Double) : ForceApplicable by objectInSpace, WithUuid by identifiableObject, SphereShape {
     init {
-        Assert.state(radian > 0.0, "radian: $radian")
+        radian.checkPositive()
     }
 
     override fun radian() = radian
