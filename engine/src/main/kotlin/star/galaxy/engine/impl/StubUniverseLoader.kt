062e26669b601c561e79c92e225d442677f4a1ea
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-22 23:26:53 +0100

    Using SphereShapeImpl

--- engine/src/main/kotlin/star/galaxy/engine/impl/StubUniverseLoader.kt
+++ engine/src/main/kotlin/star/galaxy/engine/impl/StubUniverseLoader.kt
@@ -3,10 +3,7 @@ package star.galaxy.engine.impl
 import org.springframework.stereotype.Service
 import star.galaxy.engine.Universe
 import star.galaxy.engine.UniverseLoader
-import star.galaxy.engine.entites.IdentifiableObject
-import star.galaxy.engine.entites.ObjectInSpace
-import star.galaxy.engine.entites.Orb
-import star.galaxy.engine.entites.Player
+import star.galaxy.engine.entites.*
 import java.util.*
 import javax.vecmath.Point2d
 
@@ -18,25 +15,25 @@ internal class StubUniverseLoader : UniverseLoader {
                         "sun"),
                 ObjectInSpace(1_000_000.0,
                         Point2d()),
-                100_000.0)
+                SphereShapeImpl(100_000.0))
         val earth = Orb(
                 IdentifiableObject(UUID.randomUUID().toString(),
                         "earth"),
                 ObjectInSpace(10_000.0,
                         Point2d(10_000.0, 20_000.0)),
-                5_000.0)
+                SphereShapeImpl(5_000.0))
         val mars = Orb(
                 IdentifiableObject(UUID.randomUUID().toString(),
                         "mars"),
                 ObjectInSpace(5_000.0,
                         Point2d(-10_000.0, 5_000.0)),
-                5_000.0)
+                SphereShapeImpl(5_000.0))
         val jupiter = Orb(
                 IdentifiableObject(UUID.randomUUID().toString(),
                         "jupiter"),
                 ObjectInSpace(500_000.0,
                         Point2d(-50_000.0, -70_000.0)),
-                50_000.0)
+                SphereShapeImpl(50_000.0))
         val player = Player(
                 IdentifiableObject(UUID.randomUUID().toString(), "player"),
                 ObjectInSpace(1_000.0, Point2d(3_000.0, 3_000.0)),
