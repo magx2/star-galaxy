bf9fad2ec6fe16544328b11d30a0ce9d1162bca2
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-23 19:05:02 +0100

    Changing Galaxy to SolarSystem

--- engine/src/main/kotlin/star/galaxy/engine/entites/SolarSystem.kt
+++ engine/src/main/kotlin/star/galaxy/engine/entites/SolarSystem.kt
@@ -7,10 +7,10 @@ import star.galaxy.engine.types.SphereShape
 import star.galaxy.engine.types.WithPosition
 import javax.vecmath.Vector2d
 
-class Galaxy(private val center: WithPosition,
-             @Immutable private val children: Set<ForceApplicable>,
-             private val sphereShape: SphereShape,
-             private val velocity: Vector2d = Vector2d()) :
+class SolarSystem(private val center: WithPosition,
+                  @Immutable private val children: Set<ForceApplicable>,
+                  private val sphereShape: SphereShape,
+                  private val velocity: Vector2d = Vector2d()) :
         ForceApplicables,
         SphereShape by sphereShape {
     private val mass = children.stream().mapToDouble { it.mass() }.sum()
