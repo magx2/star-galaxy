1abfb61b3915c6d327e1f248c4c4c60ea4ef4c2c
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-25 21:03:10 +0100

    Applying gravity forces

--- engine/src/main/kotlin/star/galaxy/engine/impl/PhysicsEngine.kt
+++ engine/src/main/kotlin/star/galaxy/engine/impl/PhysicsEngine.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service
 import star.galaxy.engine.Engine
 import star.galaxy.engine.Universe
 import star.galaxy.engine.metainformations.Second
+import star.galaxy.engine.physics.ForceApplicator
 import star.galaxy.engine.physics.GravityService
 import star.galaxy.engine.physics.SpaceEngineService
 import star.galaxy.engine.types.AngularApplicable
@@ -12,33 +13,19 @@ import javax.vecmath.Vector2d
 
 @Service
 class PhysicsEngine(private val gravityService: GravityService,
-                    private val spaceEngineService: SpaceEngineService) : Engine {
+                    private val spaceEngineService: SpaceEngineService,
+                    private val forceApplicator: ForceApplicator) : Engine {
     override fun nextStep(universe: Universe, @Second Δt: Int) {
-        val velocityForces: Map<ForceApplicable, Vector2d> = universe.allForceApplicables().map { it to Vector2d() }.toMap()
+        val allForceApplicables = universe.allForceApplicables()
+
+        val velocityForces: Map<ForceApplicable, Vector2d> = allForceApplicables.map { it to Vector2d() }.toMap()
 
         gravityService.applyGravity(
-                universe.allForceApplicables(),
+                allForceApplicables,
                 velocityForces,
                 universe.universeConstants())
 
-        applyVelocityForces(velocityForces, Δt)
-    }
-
-    private fun applyVelocityForces(forces: Map<ForceApplicable, Vector2d>, @Second Δt: Int) {
-        forces.forEach { (k, v) -> applyVelocityForces(k, v, Δt) }
-    }
-
-    private fun applyVelocityForces(obj: ForceApplicable, force: Vector2d, @Second Δt: Int) {
-        force.scale(obj.invertedMass()) // acceleraton
-        force.scale(Δt.toDouble())
-        obj.velocity().add(force)
-
-        // reusing force vector to store copy of velocity in it
-        force.x = obj.velocity().x
-        force.y = obj.velocity().y
-
-        force.scale(Δt.toDouble())
-        obj.position().add(force)
+        allForceApplicables.forEach { forceApplicator.apply(velocityForces[it]!!, it, Δt.toDouble()) }
     }
 
     private fun applyAngularForces(forces: Map<AngularApplicable, Double>, @Second Δt: Int) {
