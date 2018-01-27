12ad8fae295940cd3cf040adfed2b3a204503552
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-01-27 23:49:50 +0100

    Using AngularTorquesService

--- engine/src/main/kotlin/star/galaxy/engine/physics/SpaceEngineService.kt
+++ engine/src/main/kotlin/star/galaxy/engine/physics/SpaceEngineService.kt
@@ -1,13 +1,7 @@
 package star.galaxy.engine.physics
 
-import star.galaxy.engine.metainformations.Newton
-import star.galaxy.engine.types.AngularApplicable
-import star.galaxy.engine.types.ForceApplicable
 import star.galaxy.engine.types.WithForceGenerators
-import javax.vecmath.Vector2d
 
 interface SpaceEngineService {
-    fun applyForceFromForceGenerators(objects: Set<WithForceGenerators>,
-                                      velocityForces: Map<out ForceApplicable, Vector2d>,
-                                      angularTorques: MutableMap<AngularApplicable, @Newton Double>)
+    fun applyForceFromForceGenerators(objects: Collection<WithForceGenerators>)
 }
--- engine/src/main/kotlin/star/galaxy/engine/physics/impl/SpaceEngineServiceImpl.kt
+++ engine/src/main/kotlin/star/galaxy/engine/physics/impl/SpaceEngineServiceImpl.kt
@@ -1,36 +1,32 @@
 package star.galaxy.engine.physics.impl
 
 import org.springframework.stereotype.Service
+import star.galaxy.engine.AngularTorqueService
+import star.galaxy.engine.ForceService
 import star.galaxy.engine.algebra.cross
 import star.galaxy.engine.physics.SpaceEngineService
-import star.galaxy.engine.types.AngularApplicable
-import star.galaxy.engine.types.ForceApplicable
 import star.galaxy.engine.types.WithForceGenerators
-import javax.vecmath.Vector2d
 
 @Service
-internal class SpaceEngineServiceImpl : SpaceEngineService {
-    override fun applyForceFromForceGenerators(objects: Set<WithForceGenerators>,
-                                               velocityForces: Map<out ForceApplicable, Vector2d>,
-                                               angularTorques: MutableMap<AngularApplicable, Double>) {
+internal class SpaceEngineServiceImpl(private val forceService: ForceService,
+                                      private val angularTorqueService: AngularTorqueService) : SpaceEngineService {
+    override fun applyForceFromForceGenerators(objects: Collection<WithForceGenerators>) {
         objects.forEach {
-            velocity(it, velocityForces)
-            angular(it, angularTorques)
+            velocity(it)
+            angular(it)
         }
     }
 
-    private fun velocity(obj: WithForceGenerators,
-                         velocityForces: Map<out ForceApplicable, Vector2d>) {
-        val force = velocityForces[obj]!!
+    private fun velocity(obj: WithForceGenerators) {
+        val force = forceService[obj]
         obj.forceGenerators().forEach { force.add(it.velocityForce()) }
     }
 
-    private fun angular(obj: WithForceGenerators,
-                        angularTorques: MutableMap<AngularApplicable, Double>) {
-        val torque = angularTorques[obj]!! + obj.forceGenerators()
+    private fun angular(obj: WithForceGenerators) {
+        val torque = obj.forceGenerators()
                 .stream()
                 .mapToDouble { it.vectorToParentCenterOfMass() cross it.angularForce() }
                 .sum()
-        angularTorques.put(obj, torque)
+        angularTorqueService.inc(obj, torque)
     }
 }
